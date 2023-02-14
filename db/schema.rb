cac8222cbdae2eb817ded405db5692a033328573
chan <baguschandradp@gmail.com>
2023-02-14 10:11:22 +0800

    avatar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_14_012649) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_14_014206) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -49,8 +49,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_14_012649) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "pins", force: :cascade do |t|
+  create_table "posts", force: :cascade do |t|
     t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ vendor/javascript/tailwindcss--colors.js
@@ -0,0 +1,2 @@
+import{d as e}from"./_/340822e6.js";import"./_/bee160c2.js";import"picocolors";const o=e();o.__esModule;var t={};let r=o;t=(r.__esModule?r:{default:r}).default;var s=t;export{s as default};
+
